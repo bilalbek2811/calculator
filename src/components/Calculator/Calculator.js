890565c86b636f0303ee0e25b6e39e007a50bb66
bilalbek2811 <bilalbekmamadiyarov@gmail.com>
2023-10-03 16:14:04 +0600

    calculator

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,114 @@
+import React from "react";
+import "../../components/Calculator/Calculator.scss";
+const Calculator = () => {
+  return (
+    <div id="Messi">
+      <div className="container">
+        <div className="ronaldoo">
+          <div className="ronaldoo--goat">
+            <div className="ronaldoo--goat__inp">
+              <p>0</p>
+            </div>
+            <div className="ronaldoo--goat__lenght">
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn grey ">
+                  AC
+                </button>
+              </div>
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn grey ">
+                  &plusmn;
+                </button>
+              </div>{" "}
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn grey ">
+                  %
+                </button>
+              </div>{" "}
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn lens">
+                  /
+                </button>
+              </div>{" "}
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn gry">
+                  7
+                </button>
+              </div>
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn gry">
+                  8
+                </button>
+              </div>{" "}
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn gry">
+                  9
+                </button>
+              </div>{" "}
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn lens">
+                  X
+                </button>
+              </div>{" "}
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn gry">
+                  4
+                </button>
+              </div>{" "}
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn gry">
+                  5
+                </button>
+              </div>
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn  gry">
+                  6
+                </button>
+              </div>{" "}
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn lens">
+                  -
+                </button>
+              </div>
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn gry">
+                  1
+                </button>
+              </div>{" "}
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn gry">
+                  2
+                </button>
+              </div>
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn gry">
+                  3
+                </button>
+              </div>
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn lens">
+                  +
+                </button>
+              </div>
+              <div className="lol">
+                <button className="gry">0</button>
+              </div>
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn gry">
+                  .
+                </button>
+              </div>
+              <div className="ronaldoo--goat__lenght--btns">
+                <button className="ronaldoo--goat__lenght--btns__btn lens">
+                  =
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
